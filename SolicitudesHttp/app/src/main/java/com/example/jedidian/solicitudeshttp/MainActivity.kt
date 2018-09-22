62f6a071e50f14e2ed08375d3d1ec9ac2a72967e
herzh <alucin_nm@hotmail.com>
2018-09-21 20:38:17 -0500

    solicitud Get

--- SolicitudesHttp/app/src/main/java/com/example/jedidian/solicitudeshttp/MainActivity.kt
+++ SolicitudesHttp/app/src/main/java/com/example/jedidian/solicitudeshttp/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.jedidian.solicitudeshttp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,14 +18,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn_validarRed = findViewById<Button>(R.id.Btn_validarRed)
+        val btn_solicitudHttp = findViewById<Button>(R.id.Btn_solicitudHttp)
 
         btn_validarRed.setOnClickListener{
 
             //Codigo para verificar conexion a internet
             if(Network.hayRed( this)){
                 Toast.makeText(this, " si hay red! ", Toast.LENGTH_LONG).show()
-            }else{
+            }else {
                 Toast.makeText(this, "Verifica tu conexion a internet", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        btn_solicitudHttp.setOnClickListener {
+            //Codigo para verificar conexion a internet
+            if(Network.hayRed( this)){
+                Log.d( "bSolicitudOnclick", descargarDatos("http://www.google.com"))
+                Toast.makeText(this, " solicitud hecha! ", Toast.LENGTH_LONG).show()
+            }else {
+                Toast.makeText(this, "Verifica tu conexion a internet", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+    @Throws(IOException::class)
+    private fun descargarDatos(url:String):String{
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        var inputStream:InputStream? = null
+
+        try {
+
+            val url = URL(url)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
+            conn.connect()
+
+            inputStream = conn.inputStream
+
+            return inputStream.bufferedReader().use {
+                it.readText()
+            }
+
+        }finally{
+
+            if(inputStream != null){
+                inputStream.close()
             }
         }
     }
